d0d5149156a183dbdfda1dfc1342cb7e14327d65
Tanglo <Tanglo@users.noreply.github.com>
2016-01-15 12:56:00 +1100

    Initial Commit

---
+++ PepperSpray/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PepperSpray
+//
+//  Created by Lee Walsh on 15/01/2016.
+//  Copyright © 2016 Lee David Walsh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
