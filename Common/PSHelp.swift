a1ed5f07ea4e4d9b01e1edf9e525d39056eb2eb4
Tanglo <Tanglo@users.noreply.github.com>
2016-01-18 13:42:15 +1100

    pspray retrieves <key> and <destination>
    Still need to get <source>

--- Common/PSHelp.swift
+++ Common/PSHelp.swift
@@ -11,9 +11,13 @@ import Foundation
 class PSHelp{
     
     class func printHelp(){
-        print("usage: spray [-flags] <key>")
-        print("  <key>\tDeblinding key will be created at this path")
-        print("  p\tpermanent: no key file is generated")
+        print("Use pspray to blind, and wash to deblind.  By default pspray will copy files before blinding.")
+        print("usage: pspray [-flags] [ <key> | <source> | <destination> ]")
+        print("  <key>\tDeblinding key will be created at this path.  Excluded if -p flag is set")
+        print("  <destination>\tBlinded files will be placed here.  Ignored if -o flag is set")
+        print("  flags:")
+        print("    p\tpermanent: no key file is generated")
+        print("    o\toverwrite: WARNING: files will not be copied, original files will be lost!")
         print("")
         print("usage: wash [-flags] <key>")
         print("  <key>\tThe deblinding key to wash with")
--- Spray/main.swift
+++ Spray/main.swift
@@ -18,18 +18,43 @@ if arguments.count < 2 || arguments[1] == "-h"{
     let index = arguments[1].startIndex
     if arguments[1][index] == "-"{
         flags = PSFlags(arguments[1])
-        print("Main: \(flags)")
         keyPathArg++
     }
+    let createKey = !flags.contains("p")
     var keyPath = ""
-    if !flags.contains("p"){
+    if createKey{
         if (arguments.count < keyPathArg+1){
             keyPath = "./key.csv"
         } else{
             keyPath = arguments[keyPathArg]
         }
+    } else {
+        keyPathArg--
     }
-
+    let overwriteOriginals = flags.contains("o")
+    var newLocation = ""
+    if !overwriteOriginals{
+        if (arguments.count > keyPathArg+1){
+            newLocation = arguments[keyPathArg+1]
+        }
+    }
+    
+    print("Main: createKey - \(createKey)")
+    print("Main: overwriteOriginals - \(overwriteOriginals)")
     print("Main: keyPath - \(keyPath)")
+    print("Main: newLocation - \(newLocation)")
+    //Ready to go
+    var filenames = [NSURL]()
+    let workingDirectoryString = NSString(string: arguments[0]).stringByDeletingLastPathComponent
+    print("\(workingDirectoryString)")
+    let workingDirectoryURL = NSURL(fileURLWithPath: workingDirectoryString, isDirectory: true)
+    do{
+        let filenames = try NSFileManager.defaultManager().contentsOfDirectoryAtURL(NSURL(fileURLWithPath: arguments[0], isDirectory: true), includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions.SkipsHiddenFiles)
+    } catch {
+        let nsError = (error as NSError)
+        print("\(nsError.localizedDescription)")
+        exit(Int32(nsError.code))
+    }
+    print("\(filenames)")
 }
 
