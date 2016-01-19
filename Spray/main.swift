e12aadb67a0e5873618925950035e6ef97eb472a
Tanglo <Tanglo@users.noreply.github.com>
2016-01-19 18:03:18 +1100

    pspray everything ready to implement renaming files

--- Spray/main.swift
+++ Spray/main.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Lee Walsh on 15/01/2016.
 //  Copyright © 2016 Lee David Walsh. All rights reserved.
+//  This sofware is licensed under the The MIT License (MIT)
+//  See: https://github.com/Tanglo/PepperSpray/blob/master/LICENSE.md
 //
 
 import Foundation
@@ -50,11 +52,66 @@ if (arguments.count > nextArg) && (arguments[1] == "-h"){
         destinationPath = arguments[nextArg+1]
         nextArg += 2
     }
-    print("Main: flags - \(flags)")
+/*    print("Main: flags - \(flags)")
     print("Main: key? - \(createKey), keyPath - \(keyPath)")
     print("Main: sourcePath - \(sourcePath)")
     print("Main: overwrite? - \(overwriteFiles), destinationPath - \(destinationPath)")
-
+*/
+    
+    //Get the keyPath ready to create or overwrite a file
+    var isDirectory: ObjCBool = true
+    let keyFileExists = NSFileManager.defaultManager().fileExistsAtPath(keyPath, isDirectory: &isDirectory)
+//    print("\(keyFileExists), \(isDirectory)")
+    if keyFileExists{
+        if !isDirectory{
+            var response: String? = "a"
+            while (response != "y") && (response != "n"){
+                print("Overwrite existing key file: \(keyPath)? (y/n)")
+                response = readLine()
+            }
+            if response == "n"{
+                exit(0)
+            }
+        } else if keyPath.characters.last! != "/"{      //if is directory without a /
+            keyPath += "/key.csv"
+        } else{                                         //directory with a /
+            keyPath += "key.csv"
+        }
+    } else if keyPath.characters.last! == "/"{          //if user specified an non-existant path ending in a /
+        do{                                             //try to create the new directory
+            try NSFileManager.defaultManager().createDirectoryAtPath(keyPath, withIntermediateDirectories: true, attributes: nil)
+        } catch{
+            let nsError = (error as NSError)
+            print("\(nsError.localizedDescription)")
+            exit(Int32(nsError.code))
+        }
+        keyPath += "key.csv"
+    }
+//    print("keyPath: \(keyPath)")
+    
+    //Get the destination path and fileStem ready
+    let destinationExists = NSFileManager.defaultManager().fileExistsAtPath(destinationPath, isDirectory: &isDirectory)
+    var fileStem = "file"
+    if destinationExists{
+        if !isDirectory{
+            fileStem = String(NSString(string: destinationPath).lastPathComponent)
+            destinationPath = String(NSString(string: destinationPath).stringByDeletingLastPathComponent)
+        }
+    } else if destinationPath.characters.last! == "/"{          //if user specified an non-existant path ending in a /
+        do{                                             //try to create the new directory
+            try NSFileManager.defaultManager().createDirectoryAtPath(keyPath, withIntermediateDirectories: true, attributes: nil)
+        } catch{
+            let nsError = (error as NSError)
+            print("\(nsError.localizedDescription)")
+            exit(Int32(nsError.code))
+        }
+    } else{
+        fileStem = String(NSString(string: destinationPath).lastPathComponent)
+        destinationPath = String(NSString(string: destinationPath).stringByDeletingLastPathComponent)
+    }
+//    print("destStem: \(fileStem)")
+//    print("destPath: \(destinationPath)")
+    
     //Ready to go
     var filenames = [NSURL]()
     do{
@@ -65,5 +122,28 @@ if (arguments.count > nextArg) && (arguments[1] == "-h"){
         exit(Int32(nsError.code))
     }
 //    print("\(filenames)")
+    
+    var labels = [Int]()
+    for i in 0...filenames.count{
+        labels.append(i)
+    }
+//    print("\(labels)")
+    for url in filenames{
+        let fileSourcePath = url.path!
+        let fileDestinationPathStem = destinationPath + "/" + fileStem
+        let fileExtension = NSString(string: url.path!).pathExtension
+        let numberIndex = Int(arc4random_uniform(UInt32(filenames.count)))
+        let number = "\(labels[numberIndex])"
+        labels.removeAtIndex(numberIndex)
+        let numberString = number.stringByPaddingToLength(5, withString: "0", startingAtIndex: 0)
+        number.str
+        
+        print("source: \(fileSourcePath)")
+        print("destination: \(fileDestinationPathStem)")
+        print("fileExtension: \(fileExtension)")
+        print("numberString: \(numberString)")
+    }
+    
+    
 }
 
